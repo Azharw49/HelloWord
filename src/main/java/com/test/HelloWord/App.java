b5744fc6cc14f6d303d922108dadc8497fe0981f
Azhar Wahab <azharw49@gmail.com>
2022-02-18 21:25:03 +0800

    first commit

---
+++ src/main/java/com/test/HelloWord/App.java
@@ -0,0 +1,13 @@
+package com.test.HelloWord;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
